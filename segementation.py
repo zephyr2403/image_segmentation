a44911554d9c4ffdbb8164e9e75dcdfeae0e67da
zephyr2403 <ashutosh.extrimis.2403@gmail.com>
2019-02-16 00:01:50 +0530

    Implemented CallBack Function and Loop

--- segementation.py
+++ segementation.py
@@ -24,4 +24,56 @@ for i in range(10):
     colors.append(selectColor(i))
 
 
+#Global Variables
+color_marker=1
+num_marker = 10
+marks_updated = False
 
+#CALL BACK FUNCTION
+def mouseCallBack(action,x,y,flags,params):
+    global marks_updated
+
+    if action == cv2.EVENT_LBUTTONDOWN:
+        cv2.circle(marker_image,(x,y),10,(color_marker),-1)
+        
+        cv2.circle(image_copy,(x,y),10,colors[color_marker],-1)
+
+        marks_updated = True
+    
+cv2.namedWindow('Image')
+cv2.setMouseCallback('Image',mouseCallBack)
+
+while True:
+    cv2.putText(image_copy,'IIIIIIII',(3,15),cv2.FONT_HERSHEY_SIMPLEX,.5,colors[color_marker],2)
+    cv2.imshow('Segments',segments)
+    cv2.imshow('Image',image_copy)
+    
+    k = cv2.waitKey(1)
+
+    if k == 27:
+        break 
+    
+    #CLEARING COLOR 
+    elif k == ord('c'):
+        image_copy = image.copy()
+        marker_image = np.zeros(image.shape[:2],dtype=np.int32)
+        segments = np.zeros(image.shape,dtype=np.int8)
+    
+    elif k > 0 and chr(k).isdigit():
+        color_marker = int(chr(k))
+
+
+    if marks_updated:
+        
+        marker_image_copy = marker_image.copy()
+        
+        cv2.watershed(image, marker_image_copy)
+        
+        segments = np.zeros(image.shape,dtype=np.uint8)
+        
+        for color_idx in range(num_marker):
+            segments[marker_image_copy == (color_idx)] = colors[color_idx]
+        
+        marks_updated = False
+
+cv2.destroyAllWindows()
